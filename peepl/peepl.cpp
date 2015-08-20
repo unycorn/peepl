a294ee2de472b392e1d5f2a6a7e9e5b6ce40d356
unycorn <djharper2000@gmail.com>
2015-08-20 12:29:36 -0500

    Added update with death func

--- peepl/peepl.cpp
+++ peepl/peepl.cpp
@@ -4,14 +4,29 @@ int humans = 0;
 
 struct human{
 	bool   gender;
+	bool   alive;
 	float  age;
 	int    id;
 	int    kids;
 
 	void create() {
 		gender = rand() % 2;
+		alive  = true;
 		age    = 0;
 		id     = humans;
 		kids   = 0;
+
+		humans += 1;
 	}
-};
+	void kill() {
+		alive = false;
+	}
+	void update() {
+		age += 1;
+
+		if ( (rand() % 100) < (age*age)/100 ) {
+			alive = false;
+		}
+	}
+}; 
+
